442fb03d3e3f1e8015053d02399107c55c9f9e48
João Gabriel <87152738+Moura14@users.noreply.github.com>
2026-03-11 17:48:04 -0300

    feat: formatação de campos

--- lib/features/login/presentation/pages/cadastro_page.dart
+++ lib/features/login/presentation/pages/cadastro_page.dart
@@ -1,3 +1,4 @@
+import 'package:app_testes_unitarios/core/utils/input_fomrmatter.dart';
 import 'package:app_testes_unitarios/features/login/presentation/controllers/login_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
@@ -19,6 +20,10 @@ class _CadastroPageState extends State<CadastroPage> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
+  final _formKey = GlobalKey<FormState>();
+
+  bool obsecurePassword = true;
+
 
   @override
   Widget build(BuildContext context) {
@@ -26,86 +31,142 @@ class _CadastroPageState extends State<CadastroPage> {
       appBar: AppBar(
         title: const Text("Cadastro"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: nameController,
-              decoration: InputDecoration(
-                labelText: "Nome",
-                border: OutlineInputBorder(),
-              ),
-            ),
-
-            const SizedBox(height: 16),
-
-            TextField(
-              controller: phoneController,
-              keyboardType: TextInputType.phone,
-              decoration: InputDecoration(
-                labelText: "Telefone",
-                border: OutlineInputBorder(),
-              ),
-            ),
-
-            const SizedBox(height: 16),
-
-            TextField(
-              controller: emailController,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                labelText: "Email",
-                border: OutlineInputBorder(),
-              ),
-            ),
-
-            const SizedBox(height: 16),
-
-            TextField(
-              controller: passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: "Senha",
-                border: OutlineInputBorder(),
-              ),
-            ),
-
-            const SizedBox(height: 24),
-
-            Observer(
-              builder: (_) {
-                if(controller.isLoading){
-                  return CircularProgressIndicator();
-                }else{
-                  return SizedBox(
-                
-                height: 55,
-                width: double.infinity,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.black
-                    ),
-                  onPressed: () async{
-                  await  controller.cadastroUsuario(
-                      nome: nameController.text, 
-                      phone: phoneController.text, 
-                      email: emailController.text, 
-                      password: passwordController.text
-                    );
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: constraints.maxHeight),
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextFormField(
+                          validator: (value){
+                            if(value == null || value.isEmpty){
+                              return "Campo obrigatório";
+                            }
+                            return null;
+                          },
+                          controller: nameController,
+                          decoration: InputDecoration(
+                            labelText: "Nome",
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+            
+                const SizedBox(height: 16),
+            
+                TextFormField(
+                  validator: (value){
+                    if(value == null || value.isEmpty){
+                      return "Campo obrigatório";
+                    }else if(value.length < 14){
+                      return "Telefone inválido";
+                    }
+                    return null;
                   },
-                  child: const Text("Cadastrar", style: TextStyle(color: Colors.white, fontSize: 15)),
+                  controller: phoneController,
+                  keyboardType: TextInputType.phone,
+                  inputFormatters: [
+                    AppInputFormatters.phoneFormatter,
+                  ],
+                  decoration: InputDecoration(
+                    labelText: "Telefone",
+                    border: OutlineInputBorder(),
+                  ),
                 ),
-              );
-                }
-              }
-               
+            
+                const SizedBox(height: 16),
+            
+                TextFormField(
+                  validator: (value){
+                    if(value == null || value.isEmpty){
+                      return "Campo obrigatório";
+                    }else if(!value.contains('@')){
+                      return "Email inválido";
+                    }
+                    return null;
+                  },
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+            
+                const SizedBox(height: 16),
+            
+                TextFormField(
+                  validator: (value){
+                    if(value == null || value.isEmpty){
+                      return "Campo obrigatório";
+                    }else if(value.length < 6){
+                      return "A senha deve conter no mínimo 6 caracteres";
+                    }
+                    return null;
+                  },
+                  controller: passwordController,
+                  obscureText: obsecurePassword,
+                  decoration: InputDecoration(
+                    labelText: "Senha",
+                    border: OutlineInputBorder(),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        obsecurePassword ? Icons.visibility : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          obsecurePassword = !obsecurePassword;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+            
+                const SizedBox(height: 24),
+            
+                Observer(
+                  builder: (_) {
+                    if(controller.isLoading){
+                      return CircularProgressIndicator();
+                    }else{
+                      return SizedBox(
+                    
+                    height: 55,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.black
+                        ),
+                      onPressed: () async{
+                        if(_formKey.currentState?.validate() ?? false){
+                         await  controller.cadastroUsuario(
+                          nome: nameController.text, 
+                          phone: phoneController.text, 
+                          email: emailController.text, 
+                          password: passwordController.text
+                        );
+                        }
+                      },
+                      child: const Text("Cadastrar", style: TextStyle(color: Colors.white, fontSize: 15)),
+                    ),
+                  );
+                    }
+                  }
+                   
+                ),
+            
+              ],
             ),
-
-          ],
+          ),
         ),
       ),
-    );
+            )
+      );}));
   }
 }
--- lib/features/login/presentation/pages/login_page.dart
+++ lib/features/login/presentation/pages/login_page.dart
@@ -19,6 +19,10 @@ class _LoginPageState extends State<LoginPage> {
   final userNameController = TextEditingController();
   final passwordController = TextEditingController();
 
+  bool obscurePassword = false;
+
+  final _key = GlobalKey<FormState>();
+
   initState() {
     super.initState();
     
@@ -27,83 +31,119 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text('Login', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-            const SizedBox(height: 20),
-            TextField(
-              controller: userNameController,
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.email),
-                labelText: 'E-mail',
-                border: OutlineInputBorder()
-              ),
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller:  passwordController,
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.lock),
-                suffixIcon: Icon(Icons.visibility),
-                labelText: 'Senha',
-                border: OutlineInputBorder()
-              ),
-            ),
-            const SizedBox(height: 20),
-            Observer(
-              builder: (_){
-                if(controller.isLoading){
-                  return CircularProgressIndicator();
-                } 
-              
-              return SizedBox(
-                width: double.infinity,
-                height: 55,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.black
-                  ),
-                  onPressed: () async {
-                    // clear previous error
-                    controller.errorMessage = null;
-
-                    await controller.login(userNameController.text, passwordController.text);
-
-                    final error = controller.errorMessage;
-                    if (error != null && error.isNotEmpty) {
-                      print(error);
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text(error)),
-                      );
-                    } else if (controller.user != null) {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (_) => HomePage()),
-                      );
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: constraints.maxHeight),
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Form(
+                    key: _key,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text('Login', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+                        const SizedBox(height: 20),
+                        TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          validator: (value){
+                            if(value == null || value.isEmpty){
+                              return "Campo obrigatório";
+                            }
+                            return null;
+                          },
+                          controller: userNameController,
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.email),
+                            labelText: 'E-mail',
+                            border: OutlineInputBorder()
+                          ),
+                        ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  validator: (value){
+                    if(value == null || value.isEmpty){
+                      return "Campo obrigatório";
                     }
+                    return null;
                   },
- 
-                  child: Text('Login', style: TextStyle(color: Colors.white, fontSize: 15))),
-              );
-              },
-            ),
-
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => CadastroPage()),
+                  obscureText: obscurePassword,
+                  controller:  passwordController,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.lock),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        obscurePassword ? Icons.visibility : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          obscurePassword = !obscurePassword;
+                        });
+                      },
+                    ),
+                    labelText: 'Senha',
+                    border: OutlineInputBorder(),
+                  ),
+                  
+                ),
+              const SizedBox(height: 20),
+              Observer(
+                builder: (_){
+                  if(controller.isLoading){
+                    return CircularProgressIndicator();
+                  } 
+                
+                return SizedBox(
+                  width: double.infinity,
+                  height: 55,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black
+                    ),
+                    onPressed: () async {
+                      // clear previous error
+                      controller.errorMessage = null;
+                      if(_key.currentState?.validate() ?? false){
+                         await controller.login(userNameController.text, passwordController.text);
+                      }
+          
+                
+                      final error = controller.errorMessage;
+                      if (error != null && error.isNotEmpty) {
+                        print(error);
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text('Não foi possível realizar login. Tente novamente mais tarde')),
+                        );
+                      } else if (controller.user != null) {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (_) => HomePage()),
+                        );
+                      }
+                    },
+           
+                    child: Text('Login', style: TextStyle(color: Colors.white, fontSize: 15))),
                 );
-              },
-              child: Text('Não tem uma conta? Cadastre-se'),
-            )
-          ],
+                },
+              ),
+          
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => CadastroPage()),
+                  );
+                },
+                child: Text('Não tem uma conta? Cadastre-se'),
+              )
+            ],
+          ),
         ),
       ),
-    );
+    )));
+  }));
   }
 }
